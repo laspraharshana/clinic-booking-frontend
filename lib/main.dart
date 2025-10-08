852a98be486dd6bfe16573c7fd97a344925881bf
Kv23-corder <kcudugamakorala@students.nsbm.ac.lk>
2025-10-08 22:23:39 +0530

    update changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
-import 'package:clinic_booking_frontend/doctor_profile.dart';
+import 'package:clinic_booking_frontend/signin_signup.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'app.dart';
+//import 'Signinandsignup.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const ClinicApp());
+  runApp(const MyApp());
 }
 
-class ClinicApp extends StatelessWidget {
-  const ClinicApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Clinic Booking',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      home: DoctorProfilePage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      // 👇 Start app with SignIn & SignUp screen
+      home: const SignInAndSignUp(),
     );
   }
 }
