2f277ab85836e01916aa53dbc08f7bb287406a53
Kv23-corder <kcudugamakorala@students.nsbm.ac.lk>
2025-10-09 00:10:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,42 +19,9 @@ class ClinicApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Clinic Booking',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       // 👇 Start app with SignIn & SignUp screen
       home: const Dashboard(),
     );
   }
 }
-
-
-/*
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'Signinandsignup.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Clinic Booking',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // 👇 Start app with SignIn & SignUp screen
-      home: const Dashboard(),
-    );
-  }
-}
-*/
