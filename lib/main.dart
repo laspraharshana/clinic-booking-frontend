93be5e49dfabb34f2c55cd56350783f6ded03675
Kv23-corder <kcudugamakorala@students.nsbm.ac.lk>
2025-10-08 21:52:19 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,33 @@ class ClinicApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+
+/*
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'Signinandsignup.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Clinic Booking')),
-      body: const Center(child: Text('It works! Next: hook doctors list.')),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clinic Booking',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // 👇 Start app with SignIn & SignUp screen
+      home: const SignInAndSignUp(),
     );
   }
 }
+*/
